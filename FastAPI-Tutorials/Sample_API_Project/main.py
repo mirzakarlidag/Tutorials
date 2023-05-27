7ee724068b696247c8521c34b4bcd411e8e28e8c
mirzamuhammed.karlidag <mirzamuhammed.karlidag@arcelik.com>
2023-05-27 22:56:39 +0300

    title: new endpoint added desc: new endpoint added to the api to show items by id

--- FastAPI-Tutorials/Sample_API_Project/main.py
+++ FastAPI-Tutorials/Sample_API_Project/main.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from http.client import HTTPException
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -22,10 +23,19 @@ class Item(BaseModel):
 items = {
     0: Item(name="Hammer", price=9.99, count=20, id=0, category=Category.TOOLS),
     1: Item(name="Pliers", price=5.99, count=20, id=1, category=Category.TOOLS),
-    0: Item(name="Nails", price=1.99, count=100, id=0, category=Category.CONSUMABLES),
+    2: Item(name="Nails", price=1.99, count=100, id=2, category=Category.CONSUMABLES),
 }
 
 
 @app.get("/")
 def index() -> dict[str, dict[int, Item]]:
     return {"Items": items}
+
+
+@app.get("/items/{item_id}")
+def query_item_by_id(item_id: int) -> Item:
+    if item_id not in items:
+        raise HTTPException(
+            status_code=404, detail=f"Item with id:{item_id} does not exist"
+        )
+    return items[item_id]
