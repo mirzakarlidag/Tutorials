8be5110ba150a31634cb7492a93dedb667a0978f
mirzakarlidag <mirzakarlidag@gmail.com>
2023-12-10 23:14:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,34 @@
-from flask import *
-import json, time
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+items = []  # This list will store the items
 
-@app.route("/", methods=["GET"])
-def homepage():
-    data_set = {
-        "Page": "Home",
-        "Message": "Successfully loaded the homepage",
-        "Timestamp": time.time(),
-    }
-    json_dump = json.dumps(data_set)
-    return json_dump
+@app.route('/')
+def index():
+    return render_template('index.html', items=items)
 
+@app.route('/create', methods=['POST'])
+def create():
+    name = request.form['name']
+    items.append(name)
+    return redirect('/')
 
-@app.route("/user/", methods=["GET"])
-def request_page():
-    user_query = str(request.args.get("user"))  # /user/?user=username
-    data_set = {
-        "Page": "Request",
-        "Message": f"Successfully gıt the request for {user_query}",
-        "Timestamp": time.time(),
-    }
-    json_dump = json.dumps(data_set)
-    return json_dump
+@app.route('/update', methods=['POST'])
+def update():
+    old_name = request.form['old_name']
+    new_name = request.form['new_name']
+    if old_name in items:
+        index = items.index(old_name)
+        items[index] = new_name
+    return redirect('/')
 
+@app.route('/delete', methods=['POST'])
+def delete():
+    name = request.form['name']
+    if name in items:
+        items.remove(name)
+    return redirect('/')
 
-if __name__ == "__main__":
-    app.run(port=8000)
+if __name__ == '__main__':
+    app.run(debug=True)
